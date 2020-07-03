36998cda2f4409cba045d3a68cde5931a4aa6287
Katlin King <katlinking29@gmail.com>
2020-07-02 23:21:03 -0400

    added prompts that allow the user to select what type of employee they are adding to their team

--- app.js
+++ app.js
@@ -9,8 +9,141 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { prompt } = require("inquirer");
+
+const teamMembers = [];
+
+function addNewEmployee() {
+    return inquirer.prompt([
+        {
+            type: "list",
+                message: "What type of employee would you like to add to your team?",
+                name: "role", 
+                choices: [
+                    "Manager", 
+                    "Engineer",
+                    "Intern", 
+                    "I do not wish to add an employee"
+                  ]
+        }
+    ]);
+};
+
+addNewEmployee().then(function(response){
+    if(response.role === "Manager"){
+        inquirer.prompt([
+            {
+              type: "input",
+              message: "What is your name?",
+              name: "name"
+            },
+     
+           {
+             type: "input",
+             message: "What is your email?",
+             name: "email"
+           },
+     
+           {
+             type: "input",
+             message: "What is your ID?",
+             name: "id"
+           },
+     
+           {
+            type: "input",
+            message: "What is your office number?",
+            name: "officeNumber"
+           },
+         ]).then(function(response){
+            const manager = new Manager(response.name, response.email, response.id, response.officeNumber);
+            teamMembers.push(manager);    
+            console.log(teamMembers);
+            addNewEmployee();
+         })    
+    };
+
+    if(response.role === "Engineer"){
+        inquirer.prompt([
+            {
+              type: "input",
+              message: "What is your name?",
+              name: "name"
+            },
+     
+           {
+             type: "input",
+             message: "What is your email?",
+             name: "email"
+           },
+     
+           {
+             type: "input",
+             message: "What is your ID?",
+             name: "id"
+           },
+     
+           {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "github"
+           },
+         ]).then(function(response){
+            const engineer = new Engineer(response.name, response.email, response.id, response.github);
+            teamMembers.push(engineer); 
+            console.log(teamMembers);
+            addNewEmployee();
+         })
+    };
+
+    if(response.role === "Intern"){
+        inquirer.prompt([
+            {
+              type: "input",
+              message: "What is your name?",
+              name: "name"
+            },
+     
+           {
+             type: "input",
+             message: "What is your email?",
+             name: "email"
+           },
+     
+           {
+             type: "input",
+             message: "What is your ID?",
+             name: "id"
+           },
+     
+           {
+            type: "input",
+            message: "What school do you attend?",
+            name: "school"
+        }
+         ]).then(function(response){
+            const intern = new Intern(response.name, response.email, response.id, response.school);
+            teamMembers.push(intern);
+            console.log(teamMembers);
+            addNewEmployee();
+         });   
+    }
+
+    else {
+        createHTML()
+    }
+});
+// code Mike slacked out
+function createHTML() {
+    if(!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+}; 
 
 
+    
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
